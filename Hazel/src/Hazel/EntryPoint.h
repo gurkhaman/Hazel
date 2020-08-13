0b0c618ffcad17972b6ef4beb53504486bc62bbc
Arogya <kharelarogya@hotmail.com>
2020-08-13 17:19:38 +0900

    Premake done

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,12 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int arc, char** argv)
 {
+
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log");
+	int nice = 5;
+	HZ_INFO("Initialized Log, Var{0}", nice);
+	
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
